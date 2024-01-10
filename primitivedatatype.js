a9e23155fa565c7fdb564b5782aa8b9a68a85d90
Kunalsankhala <137597516+Kunalsankhala@users.noreply.github.com>
2024-01-10 20:12:42 +0530

    Add files via upload

---
+++ primitivedatatype.js
@@ -0,0 +1,9 @@
+let a = null;
+let b = 536;
+let c = true;   //can also be false
+let d = BigInt(527) + BigInt(78);
+let e = "Kunal";
+let f = Symbol("i am kunal");
+let g = undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof c) 
